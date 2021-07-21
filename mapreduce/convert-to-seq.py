35299c667ec568cf5f4bcb223433f74c86ecf3f6
Taha Tekdoğan <tahatekdogan97@hotmail.com>
2021-07-21 23:30:38 +0300

    bucket name and seq folder name generalized

--- mapreduce/convert-to-seq.py
+++ mapreduce/convert-to-seq.py
@@ -4,4 +4,4 @@
 from pyspark import SparkContext, SparkConf
 
 distFile = sc.textFile("s3://bucket-name/dataset-name.txt")
-distFile.map(lambda x: tuple(x.split(" ", 1))).saveAsSequenceFile("s3://bigdatabucket-blg564e/heartbeat-seq")
+distFile.map(lambda x: tuple(x.split(" ", 1))).saveAsSequenceFile("s3://bucket-name/seq-folder-name")
